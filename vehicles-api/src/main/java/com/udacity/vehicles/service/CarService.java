4e12779378a62111f93f2aedab8985ef03c3cb47
luanpramos <luanpramos@gmail.com>
2023-05-12 21:46:37 -0300

    fixed issued

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -75,6 +75,7 @@ public class CarService {
                         return repository.save(carToBeUpdated);
                     }).orElseThrow(CarNotFoundException::new);
         }
+
         return repository.save(car);
     }
 
